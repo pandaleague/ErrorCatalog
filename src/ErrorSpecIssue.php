d73577e0180ca44b1bce1b5db30dd82d2dda4cbf
Shaun Alberts <shaunalberts@gmail.com>
2018-03-28 20:24:30 +0200

    Add extra identifier and getters for error spec issues

--- src/ErrorSpecIssue.php
+++ src/ErrorSpecIssue.php
@@ -8,14 +8,36 @@ class ErrorSpecIssue
      * @var string $id The ID for the issue.
      */
     protected $id;
+
+    /**
+     * @var string The JSON pointer to identify the issue
+     */
+    protected $jsonPointer;
+
     /**
      * @var string $issue Use String Formatter syntax to format the issue as a parameterized string. Localize the string.
      */
     protected $issue;
 
-    public function __construct($id, $issue)
+    public function __construct($id, $jsonPointer, $issue)
     {
         $this->id = $id;
+        $this->jsonPointer = $jsonPointer;
         $this->issue = $issue;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getJsonPointer()
+    {
+        return $this->jsonPointer;
+    }
+
+    public function getIssue()
+    {
+        return $this->issue;
     }
 }
